8f35cfa5471252bedbd58be8e52fe01eb24678e0
Michael <mberriosmoreno@gmail.com>
2024-08-22 21:59:51 -0600

    Quinto Commit-Aplicando JS

--- HTML/scripts.js
+++ HTML/scripts.js
@@ -1,22 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Configuración del Intersection Observer
   const observerOptions = {
-    root: null, // Observa en el viewport del navegador
-    rootMargin: '0px',
-    threshold: 0.1 // El 10% del elemento debe estar visible para activar la animación
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('in-view');
-        observer.unobserve(entry.target); // Deja de observar una vez que el elemento está en vista
-      }
-    });
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('in-view');
+              observer.unobserve(entry.target);
+          }
+      });
   }, observerOptions);
 
-  // Observa todos los elementos con la clase .animate-on-scroll
   document.querySelectorAll('.animate-on-scroll').forEach(el => {
-    observer.observe(el);
+      observer.observe(el);
   });
 });
